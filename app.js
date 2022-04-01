09b327f8656cf14c47e69c5620354ee05c8114b3
Federico Voloschin <fedevoloschin@gmail.com>
2022-04-01 12:21:28 -0300

    listo para heroku

--- app.js
+++ app.js
@@ -39,4 +39,10 @@ app.post('/imagen',upload.single('imagen'), async (req, res) => {
    
   })
   
-  app.listen(3000)
+  const PORT = process.env.PORT || 3000
+
+  console.log({PORT})
+
+  app.listen(PORT, () => {
+    console.log('Servidor escuchando en el puerto', PORT)
+  } )
